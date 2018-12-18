24ac0cf263fcc6d74e34666df1e661874588f186
s greenbaum <gbs4ever2@gmail.com>
2018-12-18 18:11:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,9 @@ def oxford_comma(array)
 
 
 
-#if  array.count == 1
-  #array.join
-if array.count == 2
+if  array.count == 1
+  array.join
+elsif array.count == 2
   array.join(" and ")
 elsif array.count > 2
   array[-1].insert(0,"and ")
