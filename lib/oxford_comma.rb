6cf538091b5e3faa450f383ee33258d9ea162c06
s greenbaum <gbs4ever2@gmail.com>
2018-12-18 18:10:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,15 @@
 require 'pry'
 def oxford_comma(array)
-binding.pry
+
+
 
 #if  array.count == 1
   #array.join
 if array.count == 2
   array.join(" and ")
 elsif array.count > 2
-  array[-1].insert(0 "and ")
+  array[-1].insert(0,"and ")
+  binding.pry
   str =array.join (", ")
 ##str.gsub!("and")
 end
