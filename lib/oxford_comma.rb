ed57f6469964b3c36c0ed20f3c3649397c515c96
s greenbaum <gbs4ever2@gmail.com>
2018-12-18 18:09:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ binding.pry
 if array.count == 2
   array.join(" and ")
 elsif array.count > 2
-  array[-1].insert (0,"and")
+  array[-1].insert(0 "and ")
   str =array.join (", ")
 ##str.gsub!("and")
 end
